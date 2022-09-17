f94ef920057cff42c8b7d68a67dd3239d8d286ac
oyd588 <1286324547@qq.com>
2022-09-17 13:15:41 +0800

    springboot druid

--- SpringBootDruid/src/test/java/com/example/springbootdruid/SpringBootDruidApplicationTests.java
+++ SpringBootDruid/src/test/java/com/example/springbootdruid/SpringBootDruidApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.sprongbootjdbc;
+package com.example.springbootdruid;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,17 +6,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
 
 @SpringBootTest
-class SprongBootJdbcApplicationTests {
-
+class SpringBootDruidApplicationTests {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
         DataSource dataSource = jdbcTemplate.getDataSource();
         System.out.println(dataSource.getClass());
+
 
         Long aLong = jdbcTemplate.queryForObject("select count(*) from pms_brand", Long.class);
         System.out.println(aLong);
